f889778acb484922acf910dd5273f0ffb034c07a
Oisín_windows <byrne.ois@gmail.com>
2021-06-17 16:29:29 +0100

    scroll-snap-mandatory mobile test

--- src/components/ingredients/Ingredients.js
+++ src/components/ingredients/Ingredients.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const Ingredients = ({toggleAlcoholOptions, toggleMixerOptions, toggleExtraOptions, chosenAlcohol, chosenMixer, chosenExtra, toggleAlcohol, toggleMixer, toggleExtra, clearIngredients}) => {
     return ( 
-        <div className="snap snap-y snap-proximity
+        <div className="snap snap-y snap-mandatory
         h-screen w-full overflow-y-scroll overflow-x-hidden">
 
             <section className="snap-start
