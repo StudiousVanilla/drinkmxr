424e00e8a3428da977fecd51f840eb26b8ed3bf3
Oisín_windows <byrne.ois@gmail.com>
2021-06-17 19:02:13 +0100

    experimenting with ingredient page layouts

--- src/components/ingredients/Ingredients.js
+++ src/components/ingredients/Ingredients.js
@@ -17,120 +17,127 @@ const Ingredients = ({toggleAlcoholOptions, toggleMixerOptions, toggleExtraOptio
                     clear
                 </button>
             </section>
+
+
+
             
-            <section className="snap-start
-            flex flex-col h-screen w-full justify-evenly items-center bg-alcohol">
-                <button 
-                className="ingredientBtn bg-alcohol mb-10" 
+            <section className="ingredient-page-section 
+            relative flex-col p-8  
+            bg-alcohol">
+
+
+                <div 
+                className="ingredientBtn pt-9 h-full flex
+                 bg-alcohol" 
                 onClick={toggleAlcoholOptions}>
-                    Alcohol
-                </button>
-                <div className="flex flex-col">
-                    {chosenAlcohol.length <= 2 && 
-                    chosenAlcohol.map(ingredient=>
+                    <div className="flex items-end h-16">
+                        <h2 className="text-6xl">Alcohol</h2>
+                        <svg xmlns="http://www.w3.org/2000/svg" 
+                        className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+                        </svg>
+                    </div>
+                </div>
+
+
+                    {chosenAlcohol.length <= 5 && 
+                <div className="section-ingredients-list-container
+                h-1/2 w-max
+                 ">
+                    {chosenAlcohol.map(ingredient=>
                         <button 
                         onClick={()=>toggleAlcohol(ingredient)}
                         key={ingredient} 
-                        className="mb-2 w-40 mx-w-44 flex justify-between items-center bg-alcohol px-1 pl-2 rounded-xl">
+                        className="section-chosen-ingredients-list-item
+                        w-40
+                        bg-alcohol">
                             {ingredient}
-                            <div className="flex justify-center items-center bg-white rounded-full w-4 h-4">
+                            <div className="delete-icon">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
                                 <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
                                 </svg>
                             </div>
                         </button>
-                        )
-                    }
-                    {chosenAlcohol.length > 2 && 
+                        )}
+                </div>
+                    }  
+
+
+                {chosenAlcohol.length > 5 && 
+                <div className="section-ingredients-list-container
+                h-1/2 w-max
+                 ">
+
                         <button 
                         onClick={toggleAlcoholOptions}
-                        className="mb-2 w-48 mx-w-44 flex justify-between items-center bg-alcohol px-0.5 py-0.5 pl-2 rounded-2xl">
-                            {chosenAlcohol[0]} & {chosenAlcohol.length} others
-                            <div className="flex justify-center items-center bg-white rounded-full p-1">
+                        className="section-chosen-ingredients-list-item
+                        w-40
+                        bg-alcohol">
+                            {chosenAlcohol[0]} & {chosenAlcohol.length-1} others
+                            <div className="delete-icon">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
                                 <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
                                 </svg>
                             </div>
                         </button>
-                    }
                 </div>
+                    } 
+
+
+
+
+
             </section>
       
-            <section className="snap-start
-            flex flex-col h-screen w-full justify-evenly items-center bg-mixer">
-                    <button 
-                    className="ingredientBtn bg-mixer mb-10" 
-                    onClick={toggleMixerOptions}>
-                        Mixer
-                    </button>
-                    <div className="flex flex-col mt-10 -mb-44">
-                    {chosenMixer.length <= 2 && 
+            <section className="ingredient-page-section bg-mixer">
+                <button 
+                className="ingredientBtn bg-mixer" 
+                onClick={toggleMixerOptions}>
+                    Mixer
+                </button>
+                <div className="section-ingredients-list-container">
+                    {chosenMixer.length > 0 && 
                     chosenMixer.map(ingredient=>
                         <button 
                         onClick={()=>toggleMixer(ingredient)}
                         key={ingredient} 
-                        className="mb-2 w-40 mx-w-44 flex justify-between items-center bg-mixer px-1 pl-2 rounded-xl">
+                        className="section-chosen-ingredients-list-item bg-mixer">
                             {ingredient}
-                            <div className="flex justify-center items-center bg-white rounded-full w-4 h-4">
+                            <div className="delete-icon">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
                                 <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
                                 </svg>
                             </div>
                         </button>
                         )
-                    }
-                    {chosenMixer.length > 2 && 
-                        <button 
-                        onClick={toggleMixerOptions}
-                        className="mb-2 w-48 mx-w-44 flex justify-between items-center bg-mixer px-0.5 py-0.5 pl-2 rounded-2xl">
-                            {chosenMixer[0]} & {chosenMixer.length} others
-                            <div className="flex justify-center items-center bg-white rounded-full p-1">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
-                                <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    }
+                    }  
                 </div>
-                </section>
+            </section>
             
-            <section className="snap-start
-            flex flex-col h-screen w-full justify-evenly items-center bg-extra">
-                    <button 
-                    className="ingredientBtn bg-extra mb-10" 
-                    onClick={toggleExtraOptions}>
-                        Extra
-                    </button>
-                    <div className="flex flex-col mt-10 -mb-44">
-                    {chosenExtra.length <= 2 && 
+            <section className="ingredient-page-section bg-extra">
+                <button 
+                className="ingredientBtn bg-extra" 
+                onClick={toggleExtraOptions}>
+                    Extra
+                </button>
+                <div className="section-ingredients-list-container">
+                    {chosenExtra.length > 0 && 
                     chosenExtra.map(ingredient=>
                         <button 
                         onClick={()=>toggleExtra(ingredient)}
                         key={ingredient} 
-                        className="mb-2 w-40 mx-w-44 flex justify-between items-center bg-extra px-1 pl-2 rounded-xl">
+                        className="section-chosen-ingredients-list-item bg-extra">
                             {ingredient}
-                            <div className="flex justify-center items-center bg-white rounded-full w-4 h-4">
+                            <div className="delete-icon">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
                                 <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
                                 </svg>
                             </div>
                         </button>
                         )
-                    }
-                    {chosenExtra.length > 2 && 
-                        <button 
-                        onClick={toggleExtraOptions}
-                        className="mb-2 w-48 mx-w-44 flex justify-between items-center bg-extra px-0.5 py-0.5 pl-2 rounded-2xl">
-                            {chosenExtra[0]} & {chosenExtra.length} others
-                            <div className="flex justify-center items-center bg-white rounded-full p-1">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
-                                <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    }
-                    </div>
-                </section>
+                    }  
+                </div>      
+            </section>
 
         </div>
      );
